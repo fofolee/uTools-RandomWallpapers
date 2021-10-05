ab557ddca3ae1b13f0e44eb70bb461196051a16f
BigTear <54252779+BigTear@users.noreply.github.com>
2021-10-05 12:12:47 +0800

    去除了Win设置壁纸时更改契合度的选项并添加提示
    有时候不需要更改契合度（副屏竖屏的壁纸拉伸会很奇怪）

--- src/script/setDesktop.cs
+++ src/script/setDesktop.cs
@@ -3,10 +3,6 @@ using System.Runtime.InteropServices;
 using Microsoft.Win32;
 namespace Wallpaper
 {
-	public enum Style : int
-	{
-		Center, Stretch
-	}
 	public class Setter {
 		public const int	SetDesktopWallpaper	= 20;
 		public const int	UpdateIniFile		= 0x01;
@@ -14,23 +10,9 @@ namespace Wallpaper
 		[DllImport( "user32.dll", SetLastError = true, CharSet = CharSet.Auto )]
 		private static extern int SystemParametersInfo( int uAction, int uParam, string lpvParam, int fuWinIni );
 
-
-		public static void SetWallpaper( string path, Wallpaper.Style style )
+		public static void SetWallpaper( string path )
 		{
 			SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
-			RegistryKey key = Registry.CurrentUser.OpenSubKey( "Control Panel\\Desktop", true );
-			switch ( style )
-			{
-			case Style.Stretch:
-				key.SetValue( @"WallpaperStyle", "2" );
-				key.SetValue( @"TileWallpaper", "0" );
-				break;
-			case Style.Center:
-				key.SetValue( @"WallpaperStyle", "1" );
-				key.SetValue( @"TileWallpaper", "0" );
-				break;
-			}
-			key.Close();
 		}
 	}
 }
